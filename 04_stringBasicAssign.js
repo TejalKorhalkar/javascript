2780a8e9672ceba4dee129e4f722dba6d66c8c61
TejalKorhalkar <tejal.korhalkar@gmail.com>
2023-05-14 21:29:57 +0530

    String
    String

---
+++ 04_stringBasicAssign.js
@@ -0,0 +1,19 @@
+console.log("============String Basics Assignment============");
+console.log("------------------------------------------------------------------------");
+console.log("01-->");
+function stringBasics(){
+    console.log("My dream comapany is 'Samsung'");
+    console.log("------------------------------------------------------------------------");
+    console.log("02-->");
+    var hobby1 = "Reading";
+    var hobby2 = "Drawing";
+    var hobby3 = "Cooking";
+    console.log("My Hobbies:", "Hobby1:",hobby1, "Hobby2:",hobby2, "Hobby3:",hobby3);
+    var lengthHobby1 = hobby1.length;
+    var lengthHobby2 = hobby2.length;
+    var lengthHobby3 = hobby3.length;
+    var sum = lengthHobby1 + lengthHobby2 + lengthHobby3;
+    console.log("Sum of the total no. of character:", sum);
+}
+stringBasics();
+console.log("========================================================================");
